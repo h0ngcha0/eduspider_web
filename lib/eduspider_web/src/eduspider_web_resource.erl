6b7240b5319c21ff6dc85a40e4b12912a8af122b
liuhongchao <lbs.lhc@gmail.com>
2012-10-17 22:00:09 +0200

    change user_name to user_id

--- lib/eduspider_web/src/eduspider_web_resource.erl
+++ lib/eduspider_web/src/eduspider_web_resource.erl
@@ -20,23 +20,22 @@ init([]) ->
 
 to_html(ReqData, State) ->
   UserIdRes = eduspider_web_lib:get_userid(ReqData),
-  UserName  = case UserIdRes of
-                {ok, UserId0} -> get_username(UserId0);
-                false         -> false
-              end,
-  Params        = [{user_name, UserName}],
+  Params  = case UserIdRes of
+              {ok, UserId}  -> [{user_id, UserId}];
+              false         -> []
+            end,
   {ok, Content} = home_dtl:render(Params),
   {Content, ReqData, State}.
 
 %%%_* Internal Functions ===============================================
-get_username(UserId) ->
-  case user_fe:fetch(UserId) of
-    {error, notfound} ->
-      false;
-    {ok, _oUser}        ->
-      %%user_fe:get_email(User)
-      UserId
-  end.
+%% get_username(UserId) ->
+%%   case user_fe:fetch(UserId) of
+%%     {error, notfound} ->
+%%       false;
+%%     {ok, _User}        ->
+%%       %%user_fe:get_email(User)
+%%       UserId
+%%   end.
 
 %%%_* Emacs ============================================================
 %%% Local Variables:
